cc47583775e3aa4a43c3895eb2e7530ded2bd384
JefrDev <94841564+JefrDev@users.noreply.github.com>
2024-10-15 15:03:38 +0200

    -can create a message to the messaging queue now

---
+++ MessagingProducer/src/main/java/CSND/opdrachten/messaging/config/SQSConfig.java
@@ -0,0 +1,41 @@
+package CSND.opdrachten.messaging.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.BasicSessionCredentials;
+import com.amazonaws.services.sqs.AmazonSQSAsync;
+import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SQSConfig {
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    @Value("${cloud.aws.credentials.access-key}")
+    private String awsAccessKey;
+
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String awsSecretKey;
+
+    @Value("${cloud.aws.credentials.session-token}")
+    private String awsSessionToken;
+
+    @Bean
+    public QueueMessagingTemplate queueMessagingTemplate() {
+        return new QueueMessagingTemplate(amazonSQSAsync());
+    }
+
+    public AmazonSQSAsync amazonSQSAsync() {
+        return AmazonSQSAsyncClientBuilder
+                .standard()
+                .withRegion(region)
+                .withCredentials(new AWSStaticCredentialsProvider(
+                        new BasicSessionCredentials(awsAccessKey, awsSecretKey, awsSessionToken)
+                )).build();
+    }
+}
--- MessagingProducer/src/main/java/CSND/opdrachten/messaging/controller/MessagingController.java
+++ MessagingProducer/src/main/java/CSND/opdrachten/messaging/controller/MessagingController.java
@@ -1,10 +1,24 @@
 package CSND.opdrachten.messaging.controller;
 
+import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/message")
 public class MessagingController {
+    private final QueueMessagingTemplate queueMessagingTemplate;
+    private final String queueTest = "FotoTestQueue";
 
+    public MessagingController(QueueMessagingTemplate queueMessagingTemplate) {
+        this.queueMessagingTemplate = queueMessagingTemplate;
+    }
+
+    @PostMapping("")
+    public void createMessage() {
+        String messageImSending = "hallo, dit is een test of ik een message heb gestuurd.";
+
+        this.queueMessagingTemplate.convertAndSend(queueTest, messageImSending);
+    }
 }
