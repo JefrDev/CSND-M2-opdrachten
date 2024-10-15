58c2a4f6fc57adaed8b0f4eef25cf892aa989bfc
JefrDev <94841564+JefrDev@users.noreply.github.com>
2024-10-15 15:08:10 +0200

    -messagingConsumer created

---
+++ MessagingConsumer/src/test/java/csnd/opdrachten/messagingconsumer/MessagingConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package csnd.opdrachten.messagingconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
