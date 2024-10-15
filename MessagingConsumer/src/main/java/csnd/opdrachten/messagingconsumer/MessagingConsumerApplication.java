4f24163669acdabffd9bdf46d12debd3629409da
JefrDev <94841564+JefrDev@users.noreply.github.com>
2024-10-15 17:49:17 +0200

    fixed enviroment variables

--- MessagingConsumer/src/main/java/csnd/opdrachten/messagingconsumer/MessagingConsumerApplication.java
+++ MessagingConsumer/src/main/java/csnd/opdrachten/messagingconsumer/MessagingConsumerApplication.java
@@ -1,13 +1,15 @@
 package csnd.opdrachten.messagingconsumer;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MessagingConsumerApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(MessagingConsumerApplication.class, args);
+        System.out.println();
     }
-
 }
--- MessagingProducer/src/main/java/CSND/opdrachten/messaging/config/SQSConfig.java
+++ MessagingProducer/src/main/java/CSND/opdrachten/messaging/config/SQSConfig.java
@@ -9,20 +9,22 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource(value = "application.credentials.properties", ignoreResourceNotFound = true)
 public class SQSConfig {
 
     @Value("${cloud.aws.region.static}")
     private String region;
 
-    @Value("${cloud.aws.credentials.access-key}")
+    @Value("${aws.access.key}")
     private String awsAccessKey;
 
-    @Value("${cloud.aws.credentials.secret-key}")
+    @Value("${aws.secret.key}")
     private String awsSecretKey;
 
-    @Value("${cloud.aws.credentials.session-token}")
+    @Value("${aws.session.token}")
     private String awsSessionToken;
 
     @Bean
