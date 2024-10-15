521da30bf73467b80dd32fdb659812389a6028d2
JefrDev <94841564+JefrDev@users.noreply.github.com>
2024-10-15 15:09:23 +0200

    -rename

--- MessagingProducer/src/test/java/CSND/opdrachten/messaging/MessagingProducerApplicationTests.java
+++ MessagingProducer/src/test/java/CSND/opdrachten/messaging/MessagingProducerApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MessagingApplicationTests {
+class MessagingProducerApplicationTests {
 
 	@Test
 	void contextLoads() {
